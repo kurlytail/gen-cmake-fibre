6a11a73e5c3b5a4205184c2f1707daa40aa14b3b
ashwin <ashwinkn@hotmail.com>
2019-03-24 00:11:52 -0500

    generated by     - gen-lib@1.2.9     - @kurlytail/gen-sgen@1.2.7

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -1,39 +1,25 @@
 import { execSync } from 'child_process';
 
-
-
 describe('# integration test', () => {
     beforeEach(() => {
         execSync('rm -rf testoutput');
     });
 
     it('## should print help options', () => {
-        const output = execSync('./scripts/sgen-cmake-fibre.sh -h').toString();
-        expect(output).toMatchSnapshot();
-    });
-
-    it('## should generate design', () => {
-        const output = execSync('./scripts/sgen-cmake-fibre.sh -d src/test/fixture/design.js -o testoutput').toString();
-        expect(output).toMatchSnapshot();
-    });
-
-    it('## should generate design with merge', () => {
-        let output = execSync(
-            './scripts/sgen-cmake-fibre.sh -d src/test/fixture/design.js -o testoutput --overwrite=merge'
-        ).toString();
-        expect(output).toMatchSnapshot();
-        output = execSync(
-            './scripts/sgen-cmake-fibre.sh -d src/test/fixture/design.js -o testoutput --overwrite=merge'
-        ).toString();
+        let output = execSync('npm run build').toString();
+        output = execSync('sgen -g `pwd`/dist/cmake-fibre.min.js -h').toString();
         expect(output).toMatchSnapshot();
     });
 
     it('## should generate design and run cmake-fibre commands', () => {
-        let output = execSync(
-            './scripts/sgen-cmake-fibre.sh -d src/test/fixture/design.js -o testoutput --overwrite=merge'
+        let output = execSync('npm run build').toString();
+        output = execSync(
+            'sgen -g `pwd`/dist/cmake-fibre.min.js -d src/test/fixture/design.json -o testoutput'
         ).toString();
+        output = output.replace(/info: Loaded generator .*cmake-fibre.min.js.*/, '');
         expect(output).toMatchSnapshot();
         output = execSync('npm install', { cwd: 'testoutput' }).toString();
         output = execSync('npm run lint', { cwd: 'testoutput' }).toString();
+        output = execSync('npm run build', { cwd: 'testoutput' }).toString();
     });
 });
