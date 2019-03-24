70f3a7c4bb34b4bd230a6d32d54db0601848137c
ashwin <ashwinkn@hotmail.com>
2019-03-24 00:21:44 -0500

    Upgraded

--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,8 @@ basePath = Path.relative(process.cwd(), basePath) || '.';
 const options = {
     // eslint-disable-next-line no-undef
     version: app.version,
-    map: [`${basePath}/templates/map.json`]
+    map: [`${basePath}/templates/map.json`],
+    generator: ['cmake']
 };
 
 export default options;
