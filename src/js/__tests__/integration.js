4e34120a78d04c06983f87468aee85d6659bb030
Ashwin Kamath <ashwinkn@hotmail.com>
2020-04-04 12:35:55 -0500

    generated by     - gen-lib@1.3.92     - @kurlytail/gen-lib@1.3.92     - @kurlytail/gen-sgen@1.3.95     - @kurlytail/gen-log@1.3.135     - @kurlytail/gen-bst@1.3.122

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -5,18 +5,15 @@ describe('# integration test', () => {
         execSync('rm -rf testoutput');
     });
 
-    it('## should print help options', () => {
-        let output = execSync('npm run build').toString();
-        output = execSync('sgen -g `pwd`/dist/cmake-fibre.min.js -h').toString();
-        expect(output).toMatchSnapshot();
-    });
-
     it('## should generate design and run cmake-fibre commands', () => {
         let output = execSync('npm run build').toString();
         output = execSync(
             'sgen -g `pwd`/dist/cmake-fibre.min.js -d src/test/fixture/design.json -o testoutput'
         ).toString();
         output = output.replace(/info: Loaded generator .*cmake-fibre.min.js.*/, '');
+        output = output
+            .replace(/warn: Please cherrypick changes from master-sgen-generated from .*/, '')
+            .replace(/info: git cherry-pick .*/, '');
         expect(output).toMatchSnapshot();
         output = execSync('cmake CMakeLists.txt', { cwd: 'testoutput' }).toString();
         output = execSync('make', { cwd: 'testoutput' }).toString();
